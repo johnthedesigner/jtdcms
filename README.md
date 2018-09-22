f18f881ef0eb9aa178048564c81f9696587685f1
johnthedesigner <john@johnthedesigner.com>
2018-09-22 12:39:19 -0400

    Add basic redux architecture and start displaying todos from the API

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
+## JTDCMS
+
 This project is a starter setup for a Loopback-based API and a React-based client app
 
-###Goals
+### Goals
 
 * Provide a starting point for new apps that cuts down initial setup time
 * Build something that can be immediately deployed in Development, staging and production environments
 * Document setup & customization steps for myself to use in the future
 * Build only the features that I'm likely to need every time I use this regardless of the type of app
 
-###Additional Considerations
+### Additional Considerations
 
 * Prepare for particular uses that would otherwise require a lot of setup (User registration/auth, blog with categories etc...)
 * Loopback doesn't handle image/file upload, this seems like something I'll end up needing regularly
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,102 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+import { createStore, applyMiddleware } from "redux";
+import { Provider, connect } from "react-redux";
+import thunk from "redux-thunk";
+import axios from "axios";
+import "./index.css";
+// import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import _ from "lodash";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React component
+class TodoList extends React.Component {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+
+  render() {
+    const { todos } = this.props;
+    return (
+      <div>
+        {_.map(todos, (todo, index) => {
+          return <p key={index}>{todo.name}</p>;
+        })}
+      </div>
+    );
+  }
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.array.isRequired
+};
+
+// Actions
+const actionTypes = {
+  RECEIVE_TODOS: "RECEIVE_TODOS"
+};
+const fetchTodos = () => {
+  return dispatch => {
+    axios
+      .get("http://localhost:3000/api/todos")
+      .then(response => {
+        dispatch(receiveTodos(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+};
+const receiveTodos = data => {
+  return {
+    todos: data,
+    type: actionTypes.RECEIVE_TODOS
+  };
+};
+
+// Reducer
+function reducer(state = { todos: [] }, action) {
+  switch (action.type) {
+    case actionTypes.RECEIVE_TODOS:
+      return Object.assign(
+        {},
+        {
+          todos: action.todos
+        }
+      );
+    default:
+      return state;
+  }
+}
+
+// Store
+const store = createStore(reducer, applyMiddleware(thunk));
+
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    todos: state.todos
+  };
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchTodos: () => dispatch(fetchTodos())
+  };
+}
+
+// Connected Component
+const App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
