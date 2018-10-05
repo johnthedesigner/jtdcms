0719546aae262f86aefc1f0860497c91fcc73ff5
johnthedesigner <john@johnthedesigner.com>
2018-10-05 18:41:11 -0400

    start persisting application state

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
@@ -9,11 +12,21 @@ import App from "./App";
 import reducer from "./reducer";
 
 // Store
-const store = createStore(reducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
